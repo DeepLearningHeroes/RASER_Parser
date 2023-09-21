3314545737a562f2d6dbd08439cc6aa3e57b780f
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2023-09-21 19:57:41 +0530

    db_upload__endpoint_added

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, jsonify,send_file
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import os
+import io
 import parser_1
+import base64
+from pymongo import MongoClient
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 api = Api(app)
+
+client = MongoClient("mongodb+srv://chetan_325:chetan_325@cluster0.aopmq.mongodb.net/?retryWrites=true&w=majority")
+db = client["raser"] 
+collection = db["resumes"] 
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+
+    if file:
+        filename = file.filename
+        binary_data = base64.b64encode(file.read())
+
+        file_details = {
+            'filename': filename,
+            'data': binary_data.decode('utf-8')
+        }
+
+        collection.insert_one(file_details)
+
+        return jsonify({'message': 'File uploaded and saved to database.'}), 200
+    else:
+        return jsonify({'message': 'No file uploaded.'}), 400
+
+# Route to serve the PDF file
+@app.route('/file/<filename>', methods=['GET'])
+def get_file(filename):
+    file = collection.find_one({'filename': filename})
+    if file:
+        binary_data = base64.b64decode(file['data'])
+        return send_file(io.BytesIO(binary_data), mimetype='application/pdf')
+    else:
+        return jsonify({'message': 'File not found.'}), 404
+
 
 class Test(Resource):
     def get(self):
